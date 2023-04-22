3b8c6952c79ba9e92c8a3dd7aa024ee4e579544f
genamontana <predator199@yandex.ru>
2023-04-22 22:34:51 +0300

    13 lesson (in process)

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -6,14 +6,17 @@ export default {
 }
 
 const settings = {
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'API-KEY': '4874dc4c-d559-4574-8754-ba7d375822a7'
+    }
 }
 
 
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists',settings)
+        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
             .then((res) => {
                 setState(res.data)
             })
@@ -24,7 +27,7 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists',settings)
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: 'Gena TodoList'}, settings)
             .then((res) => {
                 setState(res.data)
             })
@@ -35,6 +38,10 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.delete('https://social-network.samuraijs.com/api/1.1/todo-lists/d855a927-7e16-4c06-adf5-95c84b99dd75', settings)
+            .then((res) => {
+                setState(res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
@@ -42,6 +49,10 @@ export const DeleteTodolist = () => {
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        axios.put('https://social-network.samuraijs.com/api/1.1/todo-lists/671b59eb-fbd3-4502-be3a-1186607032a9', {title: 'YoYo'}, settings)
+            .then((res) => {
+                setState(res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
